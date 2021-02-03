1d6a731f0656f901801b2c4b92165c716585f5ed
Rohidas Gowda <37271044+rohidasgowda13@users.noreply.github.com>
2021-02-03 16:08:31 +0530

    Create TicTacToe.java

---
+++ TicTacToe.java
@@ -0,0 +1,17 @@
+package internship;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		char[][] board = {{'1', '2', '3'}, 
+						  {'4', '5', '6'}, 
+						  {'7', '8', '9'}};
+		System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+		System.out.println("-+-+-");
+		System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+		System.out.println("-+-+-");
+		System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+
+	}
+
+}
